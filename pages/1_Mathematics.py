88679ec07a5bbbe331d047cc8bbe5679b897c56e
Tewodros M <tedymesfin18@gmail.com>
2024-09-24 13:57:47 +0300

    fix

--- pages/1_Mathematics.py
+++ pages/1_Mathematics.py
@@ -27,7 +27,7 @@ st.write('''
 
 st.markdown("<h3 style='text-align: left; color: black;'>Notes Outline</h3>", unsafe_allow_html=True)
 
-with st.expander("Linear transformations and matrices",expanded=True):
+with st.expander("Linear transformations and matrices",expanded=False):
     st.write('''
         Linear Algebra (Gilbert Strang & 3Blue1Brown): The series “Essence of linear Algebra” was an excellent introduction to prime myself in visualizing what all the concepts I was about to study represented. Understanding vector operations through the visualization lens of  “moving through space” made it easier to build intuition of determinants, span, and basis vectors.
 
@@ -41,7 +41,7 @@ The lectures by Gilbert Strang build foundational understanding and enable the m
     st.image("./img/column.png")
     st.write("Matrix  Method")
     st.image("./img/Matrix.png")
-with st.expander("Matrix multiplication,Chain rule",expanded=True):
+with st.expander("Matrix multiplication,Chain rule",expanded=False):
     st.write('''
     - Mulitplication and Inverse Matrices 
     
@@ -51,9 +51,9 @@ with st.expander("Matrix multiplication,Chain rule",expanded=True):
     st.write('- Loss functions and partial derivatives ')
     st.image('./img/loss.png')
 
-with st.expander("Measures of central tendency (mean, median, mode)",expanded=True):
+with st.expander("Measures of central tendency (mean, median, mode)",expanded=False):
     st.image("./img/Mean_Median_Mode.PNG")
-with st.expander("Measures of dispersion (variance, standard deviation, range)", expanded=True):
+with st.expander("Measures of dispersion (variance, standard deviation, range)", expanded=False):
     st.write('''
     - Range: 
     The simplest measure of dispersion, it's the difference between the largest and smallest values in a dataset.
@@ -63,15 +63,15 @@ with st.expander("Measures of dispersion (variance, standard deviation, range)",
     - Standard Deviation: 
     The square root of the variance. It's often preferred over variance because it's in the same units as the original data.
     ''')
-with st.expander("Basic probability concepts (independence, conditional probability)" , expanded=True):
+with st.expander("Basic probability concepts (independence, conditional probability)" , expanded=False):
     st.write('''
     - Independence: Events are independent if the occurrence of one does not affect the probability of the other. For instance, flipping a coin twice are independent events.
     - Conditional Probability: The probability of one event occurring given that another event has already occurred. For example, the probability of rain today given that it rained yesterday.
     ''')
-with st.expander("Bayes' theorem", expanded=True):
+with st.expander("Bayes' theorem", expanded=False):
     st.write("Used to determine the conditional probability of events")
     st.image("./img/Bayes.PNG")
-with st.expander("Random variables (discrete and continuous)", expanded=True):
+with st.expander("Random variables (discrete and continuous)", expanded=False):
     st.write('''
     A random variable is a numerical quantity whose value is determined by chance.
 
@@ -79,7 +79,7 @@ with st.expander("Random variables (discrete and continuous)", expanded=True):
     - Continuous Random Variables: Can take on any value within a given range like the height of a person.
     
     ''')
-with st.expander("Probability distributions (normal, binomial)", expanded=True):
+with st.expander("Probability distributions (normal, binomial)", expanded=False):
 
     st.write('''- Normal Distribution also called "bell curve," it's a symmetrical distribution where the majority of the data is clustered around the mean.''')
 
@@ -95,13 +95,13 @@ with st.expander("Probability distributions (normal, binomial)", expanded=True):
 
     st.write('''- binomial distribution is the discrete probability distribution that gives only two possible results in an experiment, either Success or Failure''')
 
-with st.expander("Hypothesis testing (null and alternative hypotheses, p-values, significance levels)", expanded=True):
+with st.expander("Hypothesis testing (null and alternative hypotheses, p-values, significance levels)", expanded=False):
     st.write('''Used to determine if there is enough evidence to support a claim.
 - Null Hypothesis: The hypothesis that there is no difference or effect.
 - Alternative Hypothesis: The hypothesis that there is a difference or effect.
 - P-value: The probability of observing a result as extreme as the one obtained, assuming the null hypothesis is true.
 - Significance Level: A threshold used to decide whether to reject or fail to reject the null hypothesis.''')
-with st.expander("Linear regression (simple and multiple) [Optional]", expanded=True):
+with st.expander("Linear regression (simple and multiple) [Optional]", expanded=False):
     st.write('''
 A statistical method used to model the relationship between a dependent variable and one or more independent variables.
 - Best fitting line for the dataset
@@ -119,31 +119,20 @@ A statistical method used to model the relationship between a dependent variable
     plt.ylim(0, 12)
 
     st.pyplot(plt)
-with st.expander("Logistic regression [Optional]", expanded=True):
+with st.expander("Logistic regression [Optional]", expanded=False):
     st.write('''- Logistic regression is used to predict the probability of a binary outcome. It's often used in classification problems.''')
     st.image("./img/logstic.PNG")
-with st.expander("Regularization Techniques", expanded=True):
+with st.expander("Regularization Techniques", expanded=False):
     st.write('''
 Are used to prevent overfitting in machine learning models. They add a penalty term to the loss function.
 - L1 Regularization or Lasso regression, it tends to shrink coefficients to zero, leading to feature selection.
 - L2 Regularization or Ridge regression, it shrinks coefficients towards zero but doesn't set them to exactly zero.
 ''')
     st.image("./img/reg.PNG")
-with st.expander("Principal Component Analysis (PCA) [Optional]", expanded=True):
+with st.expander("Principal Component Analysis (PCA) [Optional]", expanded=False):
     st.write('''A technique used to reduce the dimensionality of a dataset while preserving the most important information.''')
     st.image("./img/PCA.PNG")
-""" with st.expander("Autoregressive models [Optional]"):
-    ""
-with st.expander("Bayesian inference"):
-    ""
-with st.expander("Bayesian networks"):
-    ""
-with st.expander("Entropy and information content"):
-    ""
-with st.expander("Kullback-Leibler divergence"):
-    ""
-with st.expander("Cross-entropy"):
-    "" """
+
 
 
 
